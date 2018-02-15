a876ada3c94eba26ea157fdd30f408d190f7fa54
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-02-15 23:49:14 +0100

    Initial commit

---
+++ Crash Course/3-2. Greetings.py
@@ -0,0 +1,4 @@
+names = ['Helen', 'Lily', 'Laura']
+print('Hi, ' + names[0] + ' how are you?')
+print('Hi, ' + names[1] + ' how are you?')
+print('Hi, ' + names[2] + ' how are you?')
---
+++ Crash Course/3-3. Your Own List.py
@@ -0,0 +1,4 @@
+countries = ['Slovenia', 'Estonia', 'Iceland']
+print('I would like to travel to ' + countries[0] + ' in the summer.')
+print('I would like to travel to ' + countries[1] + ' in the summer.')
+print('I would like to travel to ' + countries[2] + ' in the summer.')

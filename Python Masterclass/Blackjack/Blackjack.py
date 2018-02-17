d2857fc196c82673d8bef9b31c3d1a5f642f8e11
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-02-17 18:13:52 +0100

    Refactored code and added New Game Function

--- Python Masterclass/Blackjack/Blackjack.py
+++ Python Masterclass/Blackjack/Blackjack.py
@@ -70,6 +70,43 @@ def deal_player():
         result_text.set('Dealer Wins!')
 
 
+def new_game():
+    global result_text
+    dealer_card_frame.destroy()
+    player_card_frame.destroy()
+    result_text.set('')
+    new_frames()
+    setup_game()
+
+def setup_game():
+    global deck
+    global dealer_hand
+    global player_hand
+    # Create a new deck of cards and shuffle them
+    deck = list(cards)
+    random.shuffle(deck)
+
+    # Create the list to store the dealer's hand and the player's hand
+    dealer_hand = []
+    player_hand = []
+
+    deal_player()
+    dealer_hand.append(deal_card(dealer_card_frame))
+    dealer_score_label.set(dealer_hand[0][0])
+    deal_player()
+
+
+def new_frames():
+    global dealer_card_frame
+    global player_card_frame
+    # Embedded frame to hold the dealer card images
+    dealer_card_frame = tkinter.Frame(card_frame, background='SpringGreen3')
+    dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=2)
+    # Embedded frame to hold the player card images
+    player_card_frame = tkinter.Frame(card_frame, background='SpringGreen3')
+    player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
+
+
 # Setup of the screen and the frame for dealer and player
 mainWindow = tkinter.Tk()
 mainWindow.title('Blackjack')
@@ -87,23 +124,17 @@ dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text='Dealer', background='SpringGreen3', fg='white').grid(row=0, column=0)
 tkinter.Label(card_frame, textvariable=dealer_score_label, background='SpringGreen3', fg='white').grid(row=1, column=0)
 
-# Embedded frame to hold the dealer card images
-dealer_card_frame = tkinter.Frame(card_frame, background='SpringGreen3')
-dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=2)
+new_frames()
 
 player_score_label = tkinter.IntVar()
 
 tkinter.Label(card_frame, text='Player', background='SpringGreen3', fg='white').grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background='SpringGreen3', fg='white').grid(row=3, column=0)
 
-# Embedded frame to hold the player card images
-player_card_frame = tkinter.Frame(card_frame, background='SpringGreen3')
-player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
+# Frame for the Buttons
 
 button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
-
-# Frame for the Buttons
 
 dealer_button = tkinter.Button(button_frame, text='Dealer', command=deal_dealer)
 dealer_button.grid(row=0, column=0)
@@ -111,21 +142,13 @@ dealer_button.grid(row=0, column=0)
 player_button = tkinter.Button(button_frame, text='Player', command=deal_player)
 player_button.grid(row= 0, column=1)
 
+new_game_button = tkinter.Button(button_frame, text='New Game', command=new_game)
+new_game_button.grid(row=0, column=2)
+
 # Loading cards
 cards = []
 load_images(cards)
 
-# Create a new deck of cards and shuffle them
-deck = list(cards)
-random.shuffle(deck)
-
-# Create the list to store the dealer's hand and the player's hand
-dealer_hand = []
-player_hand = []
-
-deal_player()
-dealer_hand.append(deal_card(dealer_card_frame))
-dealer_score_label.set(dealer_hand[0][0])
-deal_player()
+setup_game()
 
 mainWindow.mainloop()

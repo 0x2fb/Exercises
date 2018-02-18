d4c739eaf19c9721077aa67fb0f5630c07b89100
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-02-18 17:01:01 +0100

    Minor Formatting

--- Python Masterclass/Blackjack/Blackjack.py
+++ Python Masterclass/Blackjack/Blackjack.py
@@ -1,6 +1,7 @@
 import random
 import tkinter
 
+
 # Function that loads the images
 def load_images(card_images):
     suits = ['heart', 'club', 'diamond', 'spade']
@@ -21,9 +22,10 @@ def deal_card(frame):
     # Get the next card off the top of the deck
     next_card = deck.pop(0)
     # add the image to a label and display the label
-    tkinter.Label(frame, image=next_card[1], relief ='raised').pack(side='left')
+    tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
     # now return the card's face value
     return next_card
+
 
 def score_hand(hand):
     # Calculate the total score of all cards in the list
@@ -78,6 +80,7 @@ def new_game():
     new_frames()
     setup_game()
 
+
 def setup_game():
     global deck
     global dealer_hand
@@ -117,32 +120,39 @@ result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
 result.grid(row=0, column=0, columnspan=1)
 
-card_frame = tkinter.Frame(mainWindow, relief='sunken', borderwidth=1, background='SpringGreen3')
+card_frame = tkinter.Frame(mainWindow, relief='sunken',
+                           borderwidth=1, background='SpringGreen3')
 card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
-tkinter.Label(card_frame, text='Dealer', background='SpringGreen3', fg='white').grid(row=0, column=0)
-tkinter.Label(card_frame, textvariable=dealer_score_label, background='SpringGreen3', fg='white').grid(row=1, column=0)
+tkinter.Label(card_frame, text='Dealer', background='SpringGreen3',
+              fg='white').grid(row=0, column=0)
+tkinter.Label(card_frame, textvariable=dealer_score_label,
+              background='SpringGreen3', fg='white').grid(row=1, column=0)
 
 new_frames()
 
 player_score_label = tkinter.IntVar()
 
-tkinter.Label(card_frame, text='Player', background='SpringGreen3', fg='white').grid(row=2, column=0)
-tkinter.Label(card_frame, textvariable=player_score_label, background='SpringGreen3', fg='white').grid(row=3, column=0)
+tkinter.Label(card_frame, text='Player', background='SpringGreen3',
+              fg='white').grid(row=2, column=0)
+tkinter.Label(card_frame, textvariable=player_score_label,
+              background='SpringGreen3', fg='white').grid(row=3, column=0)
 
 # Frame for the Buttons
-
 button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
-dealer_button = tkinter.Button(button_frame, text='Dealer', command=deal_dealer)
+dealer_button = tkinter.Button(
+    button_frame, text='Dealer', command=deal_dealer)
 dealer_button.grid(row=0, column=0)
 
-player_button = tkinter.Button(button_frame, text='Player', command=deal_player)
-player_button.grid(row= 0, column=1)
+player_button = tkinter.Button(
+    button_frame, text='Player', command=deal_player)
+player_button.grid(row=0, column=1)
 
-new_game_button = tkinter.Button(button_frame, text='New Game', command=new_game)
+new_game_button = tkinter.Button(
+    button_frame, text='New Game', command=new_game)
 new_game_button.grid(row=0, column=2)
 
 # Loading cards

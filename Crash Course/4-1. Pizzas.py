c43e98db09c22fc55f706abfd5c47f1c81fd9855
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-02-19 21:35:10 +0100

    Initial Commit

---
+++ Crash Course/4-1. Pizzas.py
@@ -0,0 +1,7 @@
+favorite_pizzas = ['mozzarella', 'tomato', 'rucola']
+
+for p in favorite_pizzas:
+    print(f'I like {p} pizza.')
+print('I prefer vegetarian pizza.\n'
+      'I really like cheese.\n'
+      'I like to experiment with different ingredients.')

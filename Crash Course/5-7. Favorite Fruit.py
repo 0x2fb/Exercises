0613c1baa08861b224043a86ca508e4bf439d92e
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-02-27 20:14:01 +0100

    Initial Commit

---
+++ Crash Course/5-7. Favorite Fruit.py
@@ -0,0 +1,26 @@
+favorite_fruits = ['banana', 'cantaloupe', 'apple']
+
+if 'strawberry' in favorite_fruits:
+    print('You really like strawberries.')
+else:
+    print('This fruit is not available.')
+
+if 'banana' in favorite_fruits:
+    print('You really like bananas.')
+else:
+    print('This fruit is not available.')
+
+if 'mangoes' in favorite_fruits:
+    print('You really like mangoes.')
+else:
+    print('This fruit is not available.')
+
+if 'apple' in favorite_fruits:
+    print('You really like apples.')
+else:
+    print('This fruit is not available.')
+
+if 'kiwi' in favorite_fruits:
+    print('You really like kiwis.')
+else:
+    print('This fruit is not available.')

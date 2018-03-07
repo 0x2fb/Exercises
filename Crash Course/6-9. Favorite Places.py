13fb7e825e67e3d1ac73cae3d43ababec5db79d7
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-03-07 17:47:03 +0100

    Initial Commit

---
+++ Crash Course/6-9. Favorite Places.py
@@ -0,0 +1,8 @@
+favorite_places = {
+    "George": "Tokio",
+    "Marge": "San Francisco",
+    "Boris": "Moscow"
+}
+
+for k, v in favorite_places.items():
+    print(f'{k}\'s favorite place is {v}.')

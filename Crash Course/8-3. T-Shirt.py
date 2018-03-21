507b8aa98c03d27b3736564d467bd5da4187b032
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-03-21 20:10:09 +0100

    Initial Commit

---
+++ Crash Course/8-3. T-Shirt.py
@@ -0,0 +1,6 @@
+def make_shirt(size='L', text='San Jose disco club *1983'):
+    print(f"It's a size {size} shirt that has '{text}' printed on it.")
+
+
+make_shirt('S', 'Amsterdam Surf Club 1971')
+make_shirt()
---
+++ Crash Course/8-4. Large T-Shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size='L', text='I love Python.'):
+    print(f"It's a size {size} shirt that has '{text}' printed on it.")
+
+
+make_shirt()
+make_shirt('M')
+make_shirt('S', 'I also love Python.')

aed56dc4f2c102d61c42fbc179ac68add45b47da
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-04-14 08:51:57 +0200

    Refactored code

--- Web Developer Bootcamp/14-160. scorekeeper.js
+++ Web Developer Bootcamp/14-160. scorekeeper.js
@@ -1,23 +1,51 @@
 console.log("Script Connected");
-var btnp1 = document.getElementById('btnp1');
-var btnp2 = document.getElementById('btnp2');
-var scorep1 = document.getElementById('scorep1');
-var scorep2 = document.getElementById('scorep2');
-var matches = document.getElementById('matches');
-var matval = document.getElementById('number');
-var scovalp1 = 0;
-var scovalp2 = 0;
+var p1Button = document.getElementById('p1button');
+var p2Button = document.getElementById('p2button');
+var p1Display = document.getElementById('p1display');
+var p2Display = document.getElementById('p2display');
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 0;
+var numInput = document.getElementById('winningscore');
+var matchDisplay = document.getElementById('matchdisplay');
+var resetButton = document.getElementById('reset');
 
+function resetGame() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    gameOver = false;
+    p1Display.classList.remove('winner');
+    p2Display.classList.remove('winner');
+};
 
-btnp1.addEventListener('click', function () {
-    if (scovalp1 < matval & scovalp2 < matval) {
-        scovalp1 += 1;
-        scorep1.textContent = scovalp1;
+p1Button.addEventListener('click', function () {
+    if (!gameOver) {
+        p1Score++;
+        p1Display.textContent = p1Score;
+        if (p1Score === winningScore) {
+            console.log("GAME OVER");
+            gameOver = true;
+            p1Display.classList.toggle('winner');
+        }
     }
 });
-btnp2.addEventListener('click', function () {
-    if (scovalp1 < matval & scovalp2 < matval) {
-        scovalp2 += 1;
-        scorep2.textContent = scovalp2;
+p2Button.addEventListener('click', function () {
+    if (!gameOver) {
+        p2Score++;
+        p2Display.textContent = p2Score;
+        if (p2Score === winningScore) {
+            console.log("GAME OVER");
+            gameOver = true;
+            p2Display.classList.toggle('winner');
+        }
     }
+});
+resetButton.addEventListener('click', resetGame);
+numInput.addEventListener('change', function () {
+    matchDisplay.textContent = numInput.value;
+    winningScore = Number(numInput.value);
+    resetGame();
 });

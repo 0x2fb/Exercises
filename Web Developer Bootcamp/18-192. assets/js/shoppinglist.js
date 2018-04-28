356d2dcb5a16df2c03fb140b4130daf620310553
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-04-28 16:20:29 +0200

    Added CSS design

--- Web Developer Bootcamp/18-192. assets/js/shoppinglist.js
+++ Web Developer Bootcamp/18-192. assets/js/shoppinglist.js
@@ -13,6 +13,6 @@ $("input[type='text']").keypress(function (event) {
     if (event.which === 13) {
         var shopitem = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span> " + shopitem + "</li>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + shopitem + "</li>");
     };
 });

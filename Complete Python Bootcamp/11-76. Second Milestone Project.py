4526aa7d425e6c848bd9e6df1bc7be4b6cf42358
0x2fb <36317049+0x2fb@users.noreply.github.com>
2018-05-04 18:04:45 +0200

    Added Class

--- Complete Python Bootcamp/11-76. Second Milestone Project.py
+++ Complete Python Bootcamp/11-76. Second Milestone Project.py
@@ -39,6 +39,19 @@ class Deck:
         return self.deck.pop()
 
 
-test_deck = Deck()
-test_deck.shuffle()
-print(test_deck)
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
